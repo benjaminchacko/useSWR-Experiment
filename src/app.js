89afc8537168c3fb476fafbd2127781a2aec7548
Benjamin Chacko <benjaminchacko@outlook.com>
2020-03-01 08:45:02 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,25 @@
-import React from 'react'
-import useSWR, {SWRConfig} from 'swr'
+import React from "react";
+import useSWR, { SWRConfig } from "swr";
 
-const fetcher = (...args) => fetch(...args).then(res => res.json());
+const url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+const fetcher = url => fetch(url).then(res => res.json());
 
-const App = () => {
-  const url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-  const { data, error } = useSWR(url, fetcher)
-  console.log(data)
-
-  if (error){
-    return <div>Error...</div>
-  }
-
-  if (!data){
-    return <div>Loading ...</div>
-  }
+const BitcoinPrice = () => {
   
-  else return <div>{JSON.stringify(data, null, 2)}</div>
+  const { data, error } = useSWR(url);
+
+  if (error) return <div>Error...</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return <pre>{JSON.stringify(data.bpi, null, 2)}</pre>;
 };
 
-export default App
+const App = () => {
+  return (
+    <SWRConfig value={{ refreshInterval: 10000, fetcher }}>
+      <BitcoinPrice />
+    </SWRConfig>
+  );
+};
 
+export default App;
